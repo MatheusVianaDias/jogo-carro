1432196e81a2a3272f63037d6c2b50ea6c976984
MatheusVianaDias <109551825+MatheusVianaDias@users.noreply.github.com>
2022-08-26 16:06:17 -0300

    att

--- sketch.js
+++ sketch.js
@@ -1,20 +1,43 @@
 let imagemDaEstrada;
 let imagemDoAtor;
-let imagemDoCarro;
+let imagemCarro;
+//carro
+let xCarro = 850
+//yAtor
+let yAtor = 555
 
-function preload(){
-    imagemDaEstrada = loadImage("imagens/estrada.png");
-    imagemDoAtor = loadImage("imagens/ator-1.png");
-    imagemDoCarro = loadImage("imagens/carro-1.png");
+function preload() {
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png");
 }
 
 function setup() {
-    createCanvas(800, 600);
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+
+}
+
+function mostraAtor() {
+  image(imagemDoAtor, 100, yAtor, 35, 35)
+}
+
+function mostraCarro() {
+  image(imagemCarro, xCarro, 70, 70, 50)
+}
+
+function movimentaCarro() {
+  xCarro -=2;
+}
+
+function movimentaCarro(){
+  if (keyIsDown(UP_DOWN)){
+    yAtor -=3;
   }
-  
-  function draw() {
-    background(imagemDaEstrada);
-    image(imagemDoAtor, 100, 555, 35, 35);
-    image(imagemDoCarro, 500, 70, 70, 50);
-    
-  }
+}
