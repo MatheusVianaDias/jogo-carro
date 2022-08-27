1d8f7800d9cf92c745aa9211ddbaa6424e906ae5
MatheusVianaDias <109551825+MatheusVianaDias@users.noreply.github.com>
2022-08-27 20:39:19 -0300

    att

--- sketch.js
+++ sketch.js
@@ -1,15 +1,33 @@
 let imagemDaEstrada;
 let imagemDoAtor;
 let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
 //carro
 let xCarro = 850
+let yCarro = 60
+let velocidadeCarro1= 2;
+
+//carro 2
+let xCarro2 = 850
+let yCarro2 = 220
+let velocidadeCarro2= 2.5;
+
+//carro 3
+let xCarro3 = 850
+let yCarro3 = 140
+let velocidadeCarro3= 3.2;
+
 //yAtor
 let yAtor = 555
 
 function preload() {
   imagemDaEstrada = loadImage("imagens/estrada.png");
-  imagemDoAtor = loadImage("imagens/ator-1.png");
-  imagemCarro = loadImage("imagens/carro-1.png");
+  imagemDoAtor = loadImage("imagens/onepiece.png");
+  imagemCarro = loadImage("imagens/akaino.png");
+  imagemCarro2 = loadImage("imagens/aaokiji.png");
+  imagemCarro3 = loadImage("imagens/kizuru.png");
 }
 
 function setup() {
@@ -22,18 +40,35 @@ function draw() {
   mostraCarro();
   movimentaCarro();
   movimentaAtor();
+  voltaPosicaoInicialDoCarro();
 }
 
 function mostraAtor() {
-  image(imagemDoAtor, 100, yAtor, 35, 35)
+  image(imagemDoAtor, 100, yAtor, 60, 70)
 }
 
 function mostraCarro() {
-  image(imagemCarro, xCarro, 70, 70, 50)
+  image(imagemCarro, xCarro, yCarro, 80, 70)
+  image(imagemCarro2, xCarro2,yCarro2, 80, 70)
+  image(imagemCarro3, xCarro3, yCarro3, 80, 70)
 }
 
 function movimentaCarro(){
-  xCarro = xCarro - 2;
+  xCarro -= velocidadeCarro1
+  xCarro2 -= velocidadeCarro2
+  xCarro3 -= velocidadeCarro3
+}
+
+function voltaPosicaoInicialDoCarro(){
+  if (xCarro < -50){
+    xCarro = 850;
+  }
+  if (xCarro2 < -50){
+    xCarro2 = 850;
+  }
+  if (xCarro3 < -50){
+    xCarro3 = 850;
+  }
 }
 
 function movimentaAtor(){
